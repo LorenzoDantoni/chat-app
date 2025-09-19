780d515a34e4b265843dbd28fa6ec5185ce330f4
LorenzoDantoni <lorenzo.dantoni@studenti.unipd.it>
2025-09-19 15:13:31 +0200

    Added routes for signup, login, update-profile, check

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // create express app and http server
 const app = express();
@@ -12,10 +13,12 @@ const server = http.createServer(app);
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
+// routes setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
 
 // connect to MongoDB
-await connectDB()
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
